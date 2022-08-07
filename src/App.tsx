81c66df1af5b53b4e2e3b6657d8ee4190572bc47
Harish Kumar <harishsghp@gmail.com>
2022-08-07 17:10:01 +0530

    Implement initial homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import Scheduler from "./pages/Scheduler";
 
 function App() {
   return (
-    <Scheduler/>
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/scheduler" element={<Scheduler />} />
+    </Routes>
   );
 }
 
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,20 @@
+import { IconButton, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <ScheduleIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          Scheduler
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
