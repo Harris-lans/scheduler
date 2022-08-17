d046a1fc48cc3ff54d2859461e8c3f081574ccfc
Harish Kumar <harishsghp@gmail.com>
2022-08-17 22:28:28 +0530

    Link nav-bar logo to homepage

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import { IconButton, Toolbar, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
+import { Link } from 'react-router-dom';
 import LogoWhite from '../LogoWhite';
 import './index.css';
 
@@ -7,7 +8,7 @@ export default function NavBar() {
   return (
     <AppBar position="static">
       <Toolbar variant="dense" id="nav-bar">
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <IconButton component={Link} to={'/'} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <LogoWhite />
         </IconButton>
         <Typography color="inherit" component="div">
